4610c66e6ee36160e2dfd3535112db7a2f79c5c3
unusa <unusable4evar@gmail.com>
2021-10-13 20:56:38 +0200

    server advancement

--- src/battleship/BattleShip.java
+++ src/battleship/BattleShip.java
@@ -1,5 +1,6 @@
 package battleship;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
@@ -29,17 +30,20 @@ public class BattleShip extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         Settings settings = new Settings();
         //DEBUG
         NetworkBridge nwBridge = new NetworkBridge();
         nwBridge.CreateServer();
-        
-        NetworkBridge nwBridge2 = new NetworkBridge();
-        nwBridge2.ConnectServer(settings.ip + ":" + settings.port);
-        NetworkBridge nwBridge3 = new NetworkBridge();
-        nwBridge3.ConnectServer(settings.ip + ":" + settings.port);
+        //Thread t1 = new Thread(nwBridge);
+        //t1.start();
         //DEBUG
+        
+        NetworkBridge partner = new NetworkBridge();
+        partner.ConnectServer(Settings.ip, Integer.parseInt(Settings.port));
+        
+        NetworkBridge partner2 = new NetworkBridge();
+        partner2.ConnectServer(Settings.ip, Integer.parseInt(Settings.port));
         
         launch(args);
         BattleShip.quit = true;
