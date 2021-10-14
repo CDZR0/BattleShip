d929f63139ba4592da0018e13cc0c5df86ae6872
Csaba <w3x3f2@hallgato.uniduna.hu>
2021-10-14 11:06:04 +0200

    MenuGUI

--- src/battleship/BattleShip.java
+++ src/battleship/BattleShip.java
@@ -1,6 +1,7 @@
 //Csaba
 package battleship;
 
+import battleship.gui.MenuGUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
@@ -14,20 +15,7 @@ public class BattleShip extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("Hello World!");
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        MenuGUI menuGUI = new MenuGUI(primaryStage);      
     }
 
     public static void main(String[] args){
