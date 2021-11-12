9810ae5500eb85eeb7cd3ab01fcb624dbd560369
CDZR0 <vilmosmalarik@gmail.com>
2021-11-12 11:43:20 +0100

    server test fix v1

--- src/battleship/Networking/Client.java
+++ src/battleship/Networking/Client.java
@@ -40,6 +40,7 @@ public class Client implements Runnable {
                     System.out.println("fasz van");
                 }
             });
+            thread.start();
             
             while(!BattleShip.quit)
             {
