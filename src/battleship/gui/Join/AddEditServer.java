90009f503cb2eacf63bc64af5b2f7d6bcd764fb4
kapitany2 <nycs007@gmail.com>
2021-11-14 15:45:05 +0100

    Továbbfejlesztés

---
+++ src/battleship/gui/Join/AddEditServer.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship.gui.Join;
+
+import battleship.Networking.ServerAddress;
+import battleship.Resources.Resources;
+import battleship.Settings;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author nycs0
+ */
+public class AddEditServer extends JPanel {
+
+    private JTextField nameText, ipText, portText;
+    private JLabel nameLbl, ipLbl, portLbl;
+
+    public AddEditServer() {
+        this(new ServerAddress("", "", Settings.getPort()), true);
+    }
+
+    public AddEditServer(ServerAddress sa) {
+        this(sa, false);
+    }
+
+    private AddEditServer(ServerAddress sa, boolean add) {
+        setLayout(null);
+        setBackground(Resources.BackgroundColor);
+        Font font = new Font("", Font.BOLD, 12);
+        nameText = new JTextField();
+        nameText.setText(sa.getName());
+        nameText.setLocation(110, 10);
+        nameText.setSize(300, 35);
+        nameText.setBackground(Resources.BackgroundColor);
+        nameText.setFont(font);
+        this.add(nameText);
+
+        ipText = new JTextField();
+        ipText.setText(sa.getIP());
+        ipText.setLocation(110, 50);
+        ipText.setSize(300, 35);
+        ipText.setBackground(Resources.BackgroundColor);
+        ipText.setFont(font);
+        this.add(ipText);
+
+        portText = new JTextField();
+        portText.setText(sa.getPort() + "");
+        portText.setLocation(110, 90);
+        portText.setSize(50, 35);
+        portText.setBackground(Resources.BackgroundColor);
+        portText.setFont(font);
+        this.add(portText);
+
+        nameLbl = new JLabel();
+        nameLbl.setText("Server name:");
+        nameLbl.setSize(100, 35);
+        nameLbl.setLocation(10, 10);
+        this.add(nameLbl);
+
+        ipLbl = new JLabel();
+        ipLbl.setText("IP address:");
+        ipLbl.setSize(100, 35);
+        ipLbl.setLocation(10, 50);
+        this.add(ipLbl);
+
+        portLbl = new JLabel();
+        portLbl.setText("Port:");
+        portLbl.setSize(100, 35);
+        portLbl.setLocation(10, 90);
+        this.add(portLbl);
+
+        JButton save, back;
+
+        save = new JButton();
+        save.setText("Save");
+        save.setBounds(10, 200, 100, 35);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent ae) {
+                if (add) {
+                    Settings.addServer(new ServerAddress(nameText.getText(), ipText.getText(), Integer.parseInt(portText.getText())));
+                    Settings.WriteFile();
+                } else {
+                    Settings.editServer(sa.getName(),new ServerAddress(nameText.getText(), ipText.getText(), Integer.parseInt(portText.getText())));
+                    Settings.WriteFile();
+                }
+            }
+        });
+        this.add(save);
+
+        back = new JButton();
+        back.setText("Back");
+        back.setBounds(10, 245, 100, 35);
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent ae) {
+                setVisible(false);
+            }
+        });
+        this.add(back);
+    }
+
+}
--- src/battleship/gui/Join/ServerListItem.java
+++ src/battleship/gui/Join/ServerListItem.java
@@ -2,7 +2,10 @@
 package battleship.gui.Join;
 
 import battleship.Networking.ServerAddress;
+import battleship.Resources.Resources;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javafx.scene.Parent;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -18,11 +21,55 @@ public class ServerListItem extends JPanel {
 
     public ServerListItem(ServerAddress serverAddress) {
         this.serverAddress = serverAddress;
-        setBackground(new Color(50, 105, 168));
+        setBackground(Resources.BackgroundColor);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                Entered();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                Exited();
+            }
+        });
+
         JLabel name = new JLabel();
         name.setText(serverAddress.getName());
-        
         this.add(name);
+    }
+
+    public ServerAddress getServerAddress() {
+        return serverAddress;
+    }
+
+    public void Select() {
+        setBorder(BorderFactory.createLineBorder(Color.WHITE));
+    }
+
+    public void UnSelect() {
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+
+    private void Entered() {
+        int darkeningLevel = -32;
+
+        Color color = new Color(
+                getBackground().getRed() + darkeningLevel < 0 ? 0 : getBackground().getRed() + darkeningLevel,
+                getBackground().getGreen() + darkeningLevel < 0 ? 0 : getBackground().getGreen() + darkeningLevel,
+                getBackground().getBlue() + darkeningLevel < 0 ? 0 : getBackground().getBlue() + darkeningLevel
+        );
+        setBackground(color);
+    }
+
+    private void Exited() {
+        int darkeningLevel = 32;
+        Color color = new Color(
+                getBackground().getRed() + darkeningLevel > 256 ? 255 : getBackground().getRed() + darkeningLevel,
+                getBackground().getGreen() + darkeningLevel > 256 ? 255 : getBackground().getGreen() + darkeningLevel,
+                getBackground().getBlue() + darkeningLevel > 256 ? 255 : getBackground().getBlue() + darkeningLevel
+        );
+        setBackground(color);
     }
 }
