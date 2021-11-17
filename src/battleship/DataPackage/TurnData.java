bc0a9b66bab80845061172f9123b9225f994a93f
kapitany2 <nycs007@gmail.com>
2021-11-17 12:03:46 +0100

    továbbfejlesztés

--- src/battleship/DataPackage/TurnData.java
+++ src/battleship/DataPackage/TurnData.java
@@ -9,9 +9,7 @@ public class TurnData extends Data {
 
     public TurnData(int recipientID) {
         super(-1);
-        System.out.println("FA" + recipientID);
         super.recipientID = recipientID;
-        System.out.println("ClientID: " + super.clientID + " recipientID: " + super.recipientID);
     }
 
 }
