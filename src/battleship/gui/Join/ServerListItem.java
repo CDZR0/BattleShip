922162c2e6d83462abfeda46f6edbbbc9ebfddb2
kapitany2 <nycs007@gmail.com>
2021-11-20 20:26:12 +0100

    sout kiszedés

--- src/battleship/gui/Join/ServerListItem.java
+++ src/battleship/gui/Join/ServerListItem.java
@@ -67,7 +67,6 @@ public class ServerListItem extends JPanel {
             ipPort.setForeground(Color.RED);
         }
         available = value;
-        System.out.println("Available = " + value);
     }
 
     public ServerAddress getServerAddress() {
